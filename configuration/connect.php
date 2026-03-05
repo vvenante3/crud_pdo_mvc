d6edecf5ea0a6a2b69f7a99f657a1822a9bd801e
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-04 22:08:00 -0300

    inicio elaboracao da conexão com banco de dados xampp [ a complementar ]

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,36 @@
 <!-- conexão com banco de dados -->
+
+<?php
+
+    define('host', 'localhost');
+    define('databasename', 'db_pdo_mvc');
+    define('user', 'root');
+    define('password', ''); // no xampp por padrao e vazia
+
+    class Connect
+    {
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDataBase();
+        }
+
+        function connectDataBase()
+        {
+            try
+            {
+                $this->connection = new PDO('mysql:host=' . host . ';dbname=' . databasename, user, password);
+            }
+            catch(PDOException $e)
+            {
+                echo "Erro!";
+            }
+        }
+    }
+
+    
+
+
+
+?>
